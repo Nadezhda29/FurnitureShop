ed931954b42ba61d06b54041a2efcd52336d2d50
Надежда Острожинская <nadegda9966n@gmail.com>
2021-08-16 20:03:38 +0700

    Добавлена главная страница

--- FurnitureShop/Models/ViewModels/CartIndexViewModel.cs
+++ FurnitureShop/Models/ViewModels/CartIndexViewModel.cs
@@ -5,6 +5,6 @@ namespace FurnitureShop.Models.ViewModels
     public class CartIndexViewModel
     {
         public Cart Cart { get; set; }
-        //public string ReturnUrl { get; set; }
+        public string ReturnUrl { get; set; }
     }
 }
